eb915d1601fe88a502eff17b2ba1ec1e6e9068a0
Sachin <sachinharne7@gmail.com>
2020-05-29 02:36:28 +0530

    Added dashboard design

---
+++ app/src/main/java/com/coolapps/yo/maple/activity/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.coolapps.yo.maple.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coolapps.yo.maple.R;
+
+public class DashboardActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
