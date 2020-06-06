c3cd3b680d8ce12faf9a5d10a63d49c1a6d052ba
Yogesh Rathod <yogesh.rathod04@gmail.com>
2020-06-07 05:23:58 +0530

    Added gradient background to login activity

--- app/src/main/java/com/coolapps/yo/maple/activity/LoginActivity.java
+++ app/src/main/java/com/coolapps/yo/maple/activity/LoginActivity.java
@@ -40,7 +40,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.login_activity_layout);
         setToolbarTitle(R.string.login_title);
         if (LoginManager.getLoggedInUser() != null) {
             fetchFirstBatchOfFreeAndPaidData();
